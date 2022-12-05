8569f4b64da84aad2218e2eb7b34b3d7df6b5d4f
OlegF314 <o.l.e.g.f@yandex.ru>
2022-12-05 12:40:47 +0300

    Create consumer.py

---
+++ hw6/services/consumer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+import time
+import redis
+
+from parameters import REDIS_PORT, KAFKA_PORT, SERVER_PORT, WAIT_TIME
+
+
+pool = redis.ConnectionPool(host='localhost', port=REDIS_PORT, db=0)
+consumer = KafkaConsumer('task_queue')
+redis = redis.Redis(connection_pool=pool)
+
+for message in consumer:
+    print(message)
+    redis.set('task "{message}"', 'accepted')
+    time.sleep(WAIT_TIME)
+    redis.set('task "{message}"', 'done')
