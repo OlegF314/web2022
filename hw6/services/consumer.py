8932323dadcb3428676984538ebfe29db6ffe339
OlegF314 <o.l.e.g.f@yandex.ru>
2022-12-05 13:49:19 +0300

    Update consumer.py

--- hw6/services/consumer.py
+++ hw6/services/consumer.py
@@ -1,16 +1,25 @@
 from kafka import KafkaConsumer
 import time
 import redis
-
+from circuitbreaker import circuit
+from healthcheck import HealthCheck
 from parameters import REDIS_PORT, KAFKA_PORT, SERVER_PORT, WAIT_TIME
+
+def redis_correct():
+    redis_ = redis.Redis(connection_pool=pool)
 
 
 pool = redis.ConnectionPool(host='localhost', port=REDIS_PORT, db=0)
-consumer = KafkaConsumer('task_queue')
-redis = redis.Redis(connection_pool=pool)
+@circuit
+def main():
+    consumer = KafkaConsumer('task_queue')
+    redis_ = redis.Redis(connection_pool=pool)
+    health.add_check(redis_correct)
+    health.run()
+    for message in consumer:
+        print(message)
+        redis_.set(f'task "{message}"', 'accepted')
+        time.sleep(WAIT_TIME)
+        redis_.set(f'task "{message}"', 'done')
 
-for message in consumer:
-    print(message)
-    redis.set('task "{message}"', 'accepted')
-    time.sleep(WAIT_TIME)
-    redis.set('task "{message}"', 'done')
+main()
